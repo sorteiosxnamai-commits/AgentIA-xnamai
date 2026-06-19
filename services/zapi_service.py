b6f0cf7c6dfb448e57962068d46112f621983d35
Arthur Viceles <vicelesarthur@gmail.com>
2026-06-19 10:24:00 -0300

    Nova instancia

--- services/zapi_service.py
+++ services/zapi_service.py
@@ -2,14 +2,15 @@ import requests
 
 INSTANCE_ID = "3F4CB317B66E2245E7E58645B9B7D1FC"
 TOKEN = "99C8DCB079A8020DF63E7085"
-CLIENT_TOKEN = "SEU_CLIENT_TOKEN_AQUI"
+CLIENT_TOKEN = "SEU_TOKEN_COMPLETO_AQUI"
 
 def enviar_mensagem(numero, mensagem):
 
     url = f"https://api.z-api.io/instances/{INSTANCE_ID}/token/{TOKEN}/send-text"
 
     headers = {
-        "Client-Token": CLIENT_TOKEN
+        "Client-Token": CLIENT_TOKEN,
+        "Content-Type": "application/json"
     }
 
     payload = {
@@ -20,9 +21,12 @@ def enviar_mensagem(numero, mensagem):
     response = requests.post(
         url,
         json=payload,
-        headers=headers
+        headers=headers,
+        timeout=30
     )
 
+    print("URL:", url)
+    print("HEADERS:", headers)
     print("STATUS ZAPI:", response.status_code)
     print("RESPOSTA ZAPI:", response.text)
 
