7cc7e0076af7b65b5e1de45436944f1377f70308
Arthur Viceles <vicelesarthur@gmail.com>
2026-06-22 13:35:49 -0300

    Ajustes no Supabase

--- database/supabase.py
+++ database/supabase.py
@@ -4,8 +4,8 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("https://eccvlqovbjtyzbuvisba.supabase.co")
-SUPABASE_KEY = os.getenv("sb_publishable_xsafkH3rgOhZl3aSgApapQ_E1xGXgl1")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(
     SUPABASE_URL,
